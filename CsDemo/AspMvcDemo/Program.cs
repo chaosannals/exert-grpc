9d354e59f4ba645f6e5e4fb97b26215a5428b988
chaosannals <chaosannals@outlook.com>
2022-11-22 17:43:34 +0800

    AspMvcGrpc

---
+++ CsDemo/AspMvcDemo/Program.cs
@@ -0,0 +1,41 @@
+using AspMvcDemo.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddGrpc();
+builder.Services.AddCors(o => o.AddPolicy("AllowAll", builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           .WithExposedHeaders("Grpc-Status", "Grpc-Message", "Grpc-Encoding", "Grpc-Accept-Encoding");
+}));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapGrpcService<BookService>()
+    .EnableGrpcWeb()
+    .RequireCors("AllowAll");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ CsDemo/AspMvcDemo/Services/BookService.cs
@@ -0,0 +1,16 @@
+﻿using AspMvcDemo.GrpcProtos;
+using Grpc.Core;
+
+namespace AspMvcDemo.Services;
+
+public class BookService : AspMvcBookDemo.AspMvcBookDemoBase
+{
+    public override async Task<AspMvcBookListBooksReply> ListBooks(AspMvcBookListBooksRequest request, ServerCallContext context)
+    {
+        return new AspMvcBookListBooksReply
+        {
+            Code = 0,
+            Tip = "Ok",
+        };
+    }
+}
