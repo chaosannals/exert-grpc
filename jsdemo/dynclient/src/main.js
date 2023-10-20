9bec7c565e602883f90ba89d107aa5213b884505
Chen Shen Chao <chaosannals@outlook.com>
2023-10-20 11:05:06 +0800

    Update main.js

--- jsdemo/dynclient/src/main.js
+++ jsdemo/dynclient/src/main.js
@@ -3,6 +3,7 @@ import { dirname, resolve } from 'path';
 import grpc from '@grpc/grpc-js';
 import protoLoader from '@grpc/proto-loader';
 
+// 这是个 node 端示例有 node 库依赖，不可在 浏览器端 运行。
 const HERE_DIR = dirname(fileURLToPath(import.meta.url));
 const PROTO_PATH = resolve(HERE_DIR, '..', '..', 'jsdemo.proto');
 
